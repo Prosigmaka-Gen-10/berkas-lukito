7002734a394403f6cd96950b6fe66a5a998549d7
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-09-08 23:58:19 +0700

    Page Author Done

--- gen-10-react-crud-multi-page-lukito/src/pages/AuthorList.jsx
+++ gen-10-react-crud-multi-page-lukito/src/pages/AuthorList.jsx
@@ -1,6 +1,28 @@
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function AuthorList(){
+    const [authors, setAuthors] = useState([])
+
+    async function getAuthors() {
+        const res = await fetch("http://localhost:3000/authors")
+        const data = await res.json()
+        setAuthors(data)
+    }
+
+    async function deleteAuthor(authorId) {
+        await fetch('http://localhost:3000/authors/' + authorId, {
+            method: 'DELETE'
+        })
+
+        getAuthors()
+    }
+
+    useEffect(() => {
+        getAuthors()
+    }, [])
+
+
     return<>
         <h1>Daftar Author</h1>
 
@@ -10,19 +32,23 @@ function AuthorList(){
 
         <table width={"100%"} border={"1"}>
             <thead>
-            <tr>
-                <th>Nama</th>
-                <th>Action</th>
-            </tr>
+                <tr>
+                    <th>Nama</th>
+                    <th>Action</th>
+                </tr>
             </thead>
             <tbody>
-            <tr>
-                <td>Fani</td>
-                <td>
-                    <button>Edit</button>
-                    <button>Hapus</button>
-                </td>
-            </tr>
+                    {authors.map(author =>
+                        <tr key={author.id}>
+                            <td>{author.author_name}</td>
+                            <td>
+                                <Link to={"/authors/form/" + author.id}>
+                                    <button>Edit</button>
+                                </Link>
+                                <button onClick={() => deleteAuthor(author.id)}>Hapus</button>
+                            </td>
+                        </tr>
+                    )}
             </tbody>
         </table>
     </>
